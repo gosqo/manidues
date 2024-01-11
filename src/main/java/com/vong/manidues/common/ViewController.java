a1f4e8397cf3b4b2f0abe3284060bf3157b103c3
KimHyeonBong <vongright@gmail.com>
2024-01-11 15:08:30 +0900

    spring security testing begins.

---
+++ src/main/java/com/vong/manidues/common/ViewController.java
@@ -0,0 +1,22 @@
+package com.vong.manidues.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class ViewController {
+
+//    @GetMapping("/")
+//    public String getHome() {
+//        log.info("request to '/' ... ");
+//        return "index";
+//    }
+
+    @GetMapping("member/login")
+    public void viewLogin() {
+        log.info("request to '/member/login' ... ");
+    }
+
+}
---
+++ src/main/resources/static/js/member/login.js
@@ -0,0 +1,1 @@
+console.log("restarted? catch? and?");
